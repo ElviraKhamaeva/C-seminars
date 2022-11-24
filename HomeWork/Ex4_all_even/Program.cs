e401543142c8704bcf08a7464146a168da7ad912
ElviraKhamaeva <elvirakhamaeva@hotmail.com>
2022-11-25 01:28:10 +0300

    Added Homework from seminar_1

---
+++ HomeWork/Ex4_all_even/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите  число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+Console.WriteLine("Чётные числа от 1 до " + num + ":");
+
+while (i <= num)
+{
+    if (num%2 == 1 && i == num-1)
+        {
+            Console.Write(i);
+        }
+    else if (num%2 == 0 && i == num)
+        {
+            Console.Write(i);
+        }
+        else
+            {
+                Console.Write(i + ", ");
+            }
+    i+=2;
+} 
