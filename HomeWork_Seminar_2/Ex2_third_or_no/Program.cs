75079fed56bcee80be37ef662f40eb669d57f3a7
ElviraKhamaeva <elvirakhamaeva@hotmail.com>
2022-11-25 22:33:26 +0300

    Added Execises from Homework Seminar 2

---
+++ HomeWork_Seminar_2/Ex2_third_or_no/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num = new Random().Next(10,10000);
+Console.WriteLine("Число " + num);
+
+string numText = Convert.ToString(num);
+
+if (numText.Length > 2)
+{
+    Console.WriteLine("Третья цифра числа " + num + " является " + numText[2]);
+}
+else
+{
+    Console.WriteLine("У числа " + num + " нет третьей цифры");
+}
