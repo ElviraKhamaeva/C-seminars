bc6ac736c267476f3af158e6a8a871122b8ef018
ElviraKhamaeva <elvirakhamaeva@hotmail.com>
2022-11-28 02:36:51 +0300

    Added Execises from Homework Seminar 3

---
+++ HomeWork_Seminar_3/Ex1_Palindrome_5/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите  пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 9999 && num < 100000)
+{
+    string numText = Convert.ToString(num);
+    char[] arr = numText.ToCharArray();
+    Array.Reverse(arr);
+    string numTextRev = new String(arr);
+
+    Console.WriteLine("Перевертыш :" + numTextRev);
+
+    if (numText == numTextRev)
+        {
+            Console.WriteLine("Число палиндром!");
+        }
+        else
+        {
+            Console.WriteLine("Число НЕ палиндром!");
+        }    
+}
+else
+{
+    Console.WriteLine("Число не пятизначное!");
+}
---
+++ HomeWork_Seminar_3/Ex1_Palindrome_5_Var2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите  пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 9999 && num < 100000)
+{
+    string numText = Convert.ToString(num);
+    int[] numArray = new int[numText.Length];
+    for( int i=0; i< numText.Length; i++)
+        {
+            numArray[i] = int.Parse(numText[i].ToString());
+        }
+
+    Array.Reverse(numArray);
+    int[] numArrayRev = numArray;
+
+    string numTextRev = string.Concat(numArrayRev);
+    Console.WriteLine("Перевертыш: " + numTextRev);
+
+    if (numText == numTextRev)
+        {
+            Console.WriteLine("Число палиндром!");
+        }
+        else
+        {
+            Console.WriteLine("Число НЕ палиндром!");
+        }    
+}
+else
+{
+    Console.WriteLine("Число не пятизначное!");
+}
