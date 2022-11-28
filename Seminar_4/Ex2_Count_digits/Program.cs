866c9d3871873760ac3ad0db6901305e760b4f54
ElviraKhamaeva <elvirakhamaeva@hotmail.com>
2022-11-28 21:39:05 +0300

    Added Execises from Seminar 4

---
+++ Seminar_4/Ex2_Count_digits/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите  число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetCountDigits(int res)
+{
+    if (res < 0) res = - res;
+
+    int count = 0;
+    while (res > 0)
+    {
+        res /= 10;
+        count++;
+    }
+    return count;        
+}
+
+Console.WriteLine($"Количество цифр в числе {num} равно {GetCountDigits(num)}");
---
+++ Seminar_4/Ex4_Array_01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами 
+// в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+//Вариант мой
+
+// int[] GetArray(int size)
+// {
+//     int[] arr = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = new Random().Next(0,2);
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] arrPr)
+// {
+//     for (int i = 0; i < arrPr.Length; i++)
+//     {
+//         Console.Write($"{arrPr[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] array = GetArray(8);
+// PrintArray(array);
+
+
+//Вариант преподавателя
+int [] GetArray(int [] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+    return array;
+
+}
+int [] array = new int [8];
+GetArray(array);
+Console.Write(String.Join(", ", array));
