fd2de27acd220759b7e5d6f99488c73e2655838c
ElviraKhamaeva <elvirakhamaeva@hotmail.com>
2022-11-28 23:46:19 +0300

    Added Execises from Homework Seminar 4

---
+++ HomeWork_Seminar_4/Ex1_A_Exp_B/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+// число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("В какую степень возвести это число?");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double Exponentiation (int n, int k)
+{
+    double Exp = Math.Pow(n,k); 
+    return Exp;
+}
+
+Console.WriteLine($"Число {A} в степени {B} равно {Exponentiation(A, B)}");
---
+++ HomeWork_Seminar_4/Ex3_Array_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] GetArray(int [] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+    return arr;
+
+}
+int [] array = new int [8];
+GetArray(array);
+Console.Write(String.Join(", ", array));
