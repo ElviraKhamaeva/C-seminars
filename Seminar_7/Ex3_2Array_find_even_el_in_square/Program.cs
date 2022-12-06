300953f84f4536bfc14a6f250e4e0ddb92fb5a19
ElviraKhamaeva <elvirakhamaeva@hotmail.com>
2022-12-06 20:29:57 +0300

    Added Execises from Seminar 7

---
+++ Seminar_7/Ex3_2Array_find_even_el_in_square/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты
+
+Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(2, 14);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("_________________________");
+Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
